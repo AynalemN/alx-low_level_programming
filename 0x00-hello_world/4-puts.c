8dab2c2f491d66155b327f59b6c4dfbba8a5e67f
AynalemN <abebeayni@gmail.com>
2022-12-08 05:26:19 -0800

    hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
-#include<stdio.h>
-
-int main(void) {
-
+#include <stdio.h>
+/**
+ * main-entrypoint
+ * Return: positive integer(If success)
+ *
+*/
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
