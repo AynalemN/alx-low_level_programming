d38d77abb02f4b154c9d5675378359fe6bea7255
AynalemN <abebeayni@gmail.com>
2022-12-13 18:41:54 -0800

    upper and lowercase

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isalpha - check if the letter is a lower or uppercase.
+ *
+ * @c: is the letter.
+ *
+ * Return: 1 if successful else 0.
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' || "A" && c <= 'z' || 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
