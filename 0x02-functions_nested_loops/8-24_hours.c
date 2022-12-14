a2c9039471f5cafdbfa25d798c2216597637028d
AynalemN <abebeayni@gmail.com>
2022-12-13 22:50:13 -0800

    hours of the day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of Jack Bauer, starting from 00:00 to 23:59
+ * h is for hour m is for min and f is for calculating the first digit
+ * l is for calculating the last digit of a number.
+ * Return: Always  0.
+ */
+void jack_bauer(void)
+{
+	int h, m;
+
+	for (h = 0; h <= 23; h++)
+	{
+		for (m = 0; m <= 59; m++)
+		{
+			if (h <= 9)
+			{
+				_putchar('0');
+				_putchar(h + '0');
+			}
+			else if (h > 10)
+			{
+				int f = h / 10;
+				int l = h % 10;
+
+				_putchar(f + '0');
+				_putchar(l + '0');
+			}
+			if (m <= 9)
+			{
+				_putcha(':');
+				_putchar('0' + m);
+			}
+			else if (m >= 10)
+			{
+				int f = m / 10;
+				int l = m % 10;
+
+				_putchar(':');
+				_putchar(f + '0');
+				_putchar(l + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -6,3 +6,4 @@
 5. Sign - a function that prints the sign of a number.
 6. There is no such thing as absolute value in this world. You can only estimate what a thing is worth to you - a function that computes the absolute value of an integer.
 7. There are only 3 colors, 10 digits, and 7 notes; it's what we do with them that's important - a function that prints the last digit of a number.
+8. I'm federal agent Jack Bauer, and today is the longest day of my life - a function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.
