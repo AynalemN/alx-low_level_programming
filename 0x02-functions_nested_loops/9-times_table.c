9387eebb1edf00e5acbb2b3c554264dab089b6db
AynalemN <abebeayni@gmail.com>
2022-12-14 03:01:35 -0800

    time table of 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * times_table - prints the 9 times table starting with 0.
+ * i and j are the variables to do the matrix multiplication.
+ * k is a variable that holds the multiplication
+ * f and l are the first digit and last digit
+ */
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			int k;
+
+			k = i * j;
+			if (k <= 9)
+			{
+				_putchar(k + '0');
+			}
+			else if (k >= 10)
+			{
+				int f = k / 10;
+				int l = k % 10;
+
+				_putchar(f + '0');
+				_putchar(l + '0');
+			}
+			if (j != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		putchar('\n');
+	}
+}
