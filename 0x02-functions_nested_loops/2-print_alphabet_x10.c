003254836787cafe5f3e01c5d5eb29762ed25074
AynalemN <abebeayni@gmail.com>
2022-12-13 18:09:59 -0800

    printing alphabet 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 - print 10x the alphabet in lower case.
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n')
+	}
+}
