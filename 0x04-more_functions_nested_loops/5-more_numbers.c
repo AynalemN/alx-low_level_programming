7bf55d238e2c1ba4f1b8bc8716cc2ac6fe600edc
AynalemN <abebeayni@gmail.com>
2022-12-15 23:43:36 -0800

    print ten times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,14 +7,17 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (int i; i <= 9; i++)
+	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
 			if (j >= 10)
 			{
 				_putchar((j / 10) + '0');
-				_putchar((j % 0) + '0');
+			}
+			else
+			{
+				_putchar((j % 10) + '0');
 			}
 		}
 	}
