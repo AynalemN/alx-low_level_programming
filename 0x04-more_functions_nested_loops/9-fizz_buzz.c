8470579e6aa980a5caba9dc37862aa5db00d5ab5
AynalemN <abebeayni@gmail.com>
2022-12-16 02:57:22 -0800

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i < 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0)
 		{
@@ -19,7 +19,7 @@ int main(void)
 		{
 			printf("Buzz ");
 		}
-		else if (i % 3 == 0 && i % 5 == 0)
+		else if ((i % 3 == 0) && (i % 5 == 0))
 		{
 			printf("FizzBuzz ");
 		}
@@ -28,6 +28,6 @@ int main(void)
 			printf("%d ", i);
 		}
 	}
-	printf("Buzz\n");
+	printf("\n");
 	return (0);
 }
