cf829f3b98c6438ee90b9e070cd2baadd2316f69
AynalemN <abebeayni@gmail.com>
2022-12-15 20:49:37 -0800

    multiplication of 2 integers

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,12 +3,9 @@
  * mul - is a function that multiplies two integers.
  * @a: the first number
  * @b: the second number
- * Return: the multiplication result 
+ * Return: the result
  */
 int mul(int a, int b)
 {
-	int c;
-
-	c = a * b;
 	return (c);
 }
