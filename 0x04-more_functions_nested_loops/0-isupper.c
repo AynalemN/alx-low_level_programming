8c6ce2aa83e9a76e9e02e63b6c624e1fd98858cb
AynalemN <abebeayni@gmail.com>
2022-12-15 20:06:27 -0800

    is uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isupper - checks for uppercase
+ * 
+ * @c - is the alphabet or the argument checked
+ * 
+ * Return: 1 if c is uppercase else 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
