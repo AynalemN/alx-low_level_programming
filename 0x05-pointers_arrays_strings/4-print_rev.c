b88fc762194d8f459afdde983ee282877e0134ab
AynalemN <abebeayni@gmail.com>
2022-12-20 04:40:24 -0800

    reverse a string

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,2 +1,26 @@
 #include "main.h"
 
+/**
+ * print_rev - prints a string, in reverse
+ * @s: is the pointer argumnet passed
+ * n is the counter or the length
+ */
+void print_rev(char *s)
+{
+	int i, j;
+	int n = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		n++;
+	}
+	for (j = n - 1;;)
+	{
+		if (j >= 0)
+		{
+			_putchar(s[j]);
+			j--;
+		}
+	}
+	_putchar('\n');
+}
