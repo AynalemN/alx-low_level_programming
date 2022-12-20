d6ca3afabec314ef55f9cd48d3cbd90458bfd5db
AynalemN <abebeayni@gmail.com>
2022-12-19 22:15:56 -0800

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * puts2- prints a string on even places
+ *
+ * @str: is pointer parameter passed
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
