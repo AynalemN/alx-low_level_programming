4a4057ae0643bd0ded035606e0d8eef2ad5a4a1e
AynalemN <abebeayni@gmail.com>
2023-01-06 01:41:47 -0800

    passed argument

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -3,11 +3,12 @@
  * main - prints all the passed arguments
  * @argc: argument count
  * @argv: passed array of strings
- * Return: 0 if successful 
+ * Return: 0 if successful
  */
 int main(int argc, char *argv[])
 {
 	int i;
+
 	if (argc >= 1)
 	{
 		for (i = 0; i < argc; i++)
