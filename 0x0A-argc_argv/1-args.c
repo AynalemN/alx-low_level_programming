eba93baa9da7c05af6117ca947c74f28a5e60c27
AynalemN <abebeayni@gmail.com>
2023-01-06 01:18:36 -0800

    count argument

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main- to count the number of arguments passed
+ * @argc: number of command line arguments
+ * @argv: a string of array passed
+ * Return: 0 if successsful
+ */
+int main(int argc, char **argv __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
