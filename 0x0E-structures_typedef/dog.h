d295812a6746cf7900d3393344fae185da53674e
AynalemN <abebeayni@gmail.com>
2023-01-16 01:28:39 -0800

    struct defination

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * struct dog - holds mebers of stuct dog
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
