57bbecee79da155eb5c5bc66f18ab2793c110d55
AynalemN <abebeayni@gmail.com>
2023-01-18 01:54:08 -0800

    update

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdlib.h>
 /**
  * print_name - prints a name
  * @name:pointer to string litral
@@ -7,8 +8,9 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name && f)
+	if (name == NULL || f == NULL)
 	{
-		f(name);
+		return;
 	}
+	f(name);
 }
