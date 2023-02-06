f3dd8bd4d32adf0eb4c6424421587589d75f9e95
AynalemN <abebeayni@gmail.com>
2023-02-06 07:33:41 -0800

    copy contnet of a file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -20,7 +20,7 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	file_to_FD = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 664);
+	file_to_FD = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (file_to_FD == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
